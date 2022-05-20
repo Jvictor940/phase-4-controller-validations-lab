98b6d5352ea34274b4e28ba846e6d74e631f3bca
Jerry <Jvictor940@gmail.com>
2022-05-19 22:57:35 -0400

    commit tree b83824ef812824672f476c74cabd16b655f5dc64

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,7 @@
 class AuthorsController < ApplicationController
-  
+  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessible_entity_response
+
   def show
     author = Author.find(params[:id])
 
@@ -8,8 +10,10 @@ class AuthorsController < ApplicationController
 
   def create
     author = Author.create(author_params)
-
     render json: author, status: :created
+    # render json: { errors: invalid.record.errors.full_message }, status: :unprocessible_entity
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -17,5 +21,9 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+
+  # def render_unprocessible_entity_response(invalid)
+  #   render json: { errors: invalid.record.errors.full_message }, status: :unprocessible_entity
+  # end
   
 end
